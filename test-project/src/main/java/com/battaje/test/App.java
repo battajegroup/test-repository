c8aca17e3495ea52b78848e67854c0863e56cea3
battajegroup <bharathraob@gmail.com>
2017-03-02 20:58:38 -0500

    COMMIT

--- test-project/src/main/java/com/battaje/test/App.java
+++ test-project/src/main/java/com/battaje/test/App.java
@@ -8,9 +8,13 @@ public class App
 {
     public static void main( String[] args )
     {
+
         System.out.println( "Hello World!" );   
         
         System.out.println( "This is added in defect 321" );   
+        
+        System.out.println( "Added in the branch 123" );
+
         
     }
 }
